8205bd82731c07ee1d30eb576744a0ea27dd1c5f
lkjsxc <lkjsxc@gmail.com>
2025-05-26 23:59:15 +0000

    Refactor input handling in abc407_c.c to use a larger buffer and improve string processing logic

--- kyopuro/abc407_c.c
+++ kyopuro/abc407_c.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-char s[100002];
+char s_data[500002];
 
 int main() {
-    scanf("%s", s);
+    char* s = s_data + 1;
+    s_data[0] = '\0';
+    int scan_result = scanf("%s", s);
     int s_len = strlen(s);
     int ans = 0;
     int sum = 0;
-    for (int i = s_len - 1; i >= 0; i--) {
+    for (int i = s_len - 1; s[i] != '\0'; i--) {
         while (1) {
             int digit = s[i] - '0';
             if ((10 + digit - (sum % 10)) % 10 == 0) {
