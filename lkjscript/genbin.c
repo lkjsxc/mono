475b7085607036bec4221b52cc693778a2ab9edb
lkjsxc <lkjsxc@gmail.com>
2025-05-27 09:41:19 +0000

    Refactor genbin function to improve nodetype handling and update main script to correct return statement

--- lkjscript/genbin.c
+++ lkjscript/genbin.c
@@ -4,6 +4,17 @@ result_t genbin(node_t* execlist_rbegin, uint8_t* bin) {
     uint8_t* bin_itr = bin;
     node_t* node_itr = execlist_rbegin;
     while (node_itr != NULL) {
+        switch (node_itr->nodetype) {
+            case NODETYPE_NOP:
+                break;
+            case NODETYPE_PUSH_CONST:
+                *(uint8_t*)bin_itr++ = NODETYPE_PUSH_CONST;
+                *(int64_t*)bin_itr++ = node_itr->val;
+                break;
+            default:
+                ERROUT;  // Unsupported nodetype
+                return ERR;
+        }
         node_itr = node_itr->next;
     }
     return OK;
