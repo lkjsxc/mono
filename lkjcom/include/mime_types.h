fef49527f3c5dbac6576c6912183dd11b3d10d8b
lkjsxc <lkjsxc@gmail.com>
2025-07-06 10:44:19 +0900

    Remove unused HTML, CSS, JavaScript, and server-related source files
    - Deleted 500.html and index.html as they are no longer needed.
    - Removed script.js and style.css which were associated with the deleted HTML files.
    - Eliminated config.c, db.c, file_handler.c, http_parser.c, http_response.c, json_parser.c, main.c, mime_types.c, request_handler.c, server_socket.c, and utils.c as part of the server code refactor.
    - Added a Dockerfile for building the application with a static binary.

--- lkjcom/include/mime_types.h
+++
@@ -1,6 +0,0 @@
-#ifndef MIME_TYPES_H
-#define MIME_TYPES_H
-
-const char* mime_get_type(const char *filename);
-
-#endif
--- lkjcom/include/server_socket.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_SOCKET_H
-#define SERVER_SOCKET_H
-
-int server_socket_create(int port);
-int server_socket_accept(int server_fd);
-void server_socket_close(int socket_fd);
-
-#endif
