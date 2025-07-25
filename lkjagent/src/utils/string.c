43aeb68aa80768c0070d5a91a341570e61849ab5
lkjsxc <lkjsxc@gmail.com>
2025-07-25 12:39:24 +0000

    feat: Add string_clean function for resetting string contents

--- lkjagent/src/utils/string.c
+++ lkjagent/src/utils/string.c
@@ -28,11 +28,20 @@ result_t string_create_str(pool_t* pool, string_t** string, const char* str) {
     return RESULT_OK;
 }
 
+result_t string_clean(pool_t* pool, string_t* string) {
+    if (pool_string_realloc(pool, &string, 16) != RESULT_OK) {
+        RETURN_ERR("Failed to reallocate string to clean it");
+    }
+    if (string->size > 0) {
+        string->size = 0;
+        string->data[0] = '\0';
+    }
+    return RESULT_OK;
+}
+
 result_t string_copy_string(pool_t* pool, string_t** string1, const string_t* string2) {
-    if (string2->size + 1 > (*string1)->capacity) {
-        if (pool_string_realloc(pool, string1, string2->size + 1) != RESULT_OK) {
-            RETURN_ERR("Failed to reallocate string with sufficient capacity");
-        }
+    if (pool_string_realloc(pool, string1, string2->capacity) != RESULT_OK) {
+        RETURN_ERR("Failed to reallocate string with sufficient capacity");
     }
     (*string1)->size = string2->size;
     memcpy((*string1)->data, string2->data, string2->size + 1);
@@ -41,10 +50,8 @@ result_t string_copy_string(pool_t* pool, string_t** string1, const string_t* st
 
 result_t string_copy_str(pool_t* pool, string_t** string, const char* str) {
     size_t len = strlen(str);
-    if (len + 1 > (*string)->capacity) {
-        if (pool_string_realloc(pool, string, len + 1) != RESULT_OK) {
-            RETURN_ERR("Failed to reallocate string with sufficient capacity");
-        }
+    if (pool_string_realloc(pool, string, len + 1) != RESULT_OK) {
+        RETURN_ERR("Failed to reallocate string with sufficient capacity");
     }
     (*string)->size = len;
     memcpy((*string)->data, str, len + 1);
@@ -75,8 +82,8 @@ result_t string_append_str(pool_t* pool, string_t** string, const char* str) {
 }
 
 result_t string_append_char(pool_t* pool, string_t** string, char c) {
-    if ((*string)->size + 1 >= (*string)->capacity) {
-        if (pool_string_realloc(pool, string, (*string)->size + 2) != RESULT_OK) {
+    if ((*string)->size + 1 + 1 >= (*string)->capacity) {
+        if (pool_string_realloc(pool, string, (*string)->size + 1 + 1) != RESULT_OK) {
             RETURN_ERR("Failed to reallocate string with sufficient capacity");
         }
     }
