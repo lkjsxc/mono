e670ec95a216a01cc6b8212acfdf74b0c50872ce
lkjsxc <lkjsxc@gmail.com>
2025-09-10 16:46:05 +0900

    feat: Refactor assembly code handling and improve error reporting in lkjscript

--- lkjscript/src/lkjscript.c
+++ lkjscript/src/lkjscript.c
@@ -1,172 +1,126 @@
+#define _GNU_SOURCE
+#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/mman.h>
 
-#define LKJSCRIPT_SIZE (1024 * 1024 * 16)
-#define LKJSCRIPT_SRCPATH "/data/0001.lkjscript"
-
-typedef enum inst_t inst_t;
-typedef struct lkjscript_t lkjscript_t;
-typedef struct instmap_t instmap_t;
-
-enum inst_t {
-    INST_NULL,
-    INST_END,
-    INST_DEBUG,
-    INST_IMM,
-    INST_COPY,
-};
-
-struct lkjscript_t {
-    uint8_t data[LKJSCRIPT_SIZE];
-};
-
-struct instmap_t {
-    const char* name;
-    inst_t inst;
-};
-
-static instmap_t instmap[] = {
-    {"null", INST_NULL},
-    {"end", INST_END},
-    {"debug", INST_DEBUG},
-    {"imm", INST_IMM},
-    {"copy", INST_COPY},
-};
-
-static lkjscript_t lkjscript;
-
-int32_t token_size(const char* token) {
-    for (int32_t i = 0; 1; i++) {
-        if (token[i] == ' ' || token[i] == '\n' || token[i] == '\r' || token[i] == '\t' || token[i] == '\0') {
-            return i;
-        }
+int assemble_arm64(unsigned char* code_buf, const char* assembly_source) {
+    char* source_copy = strdup(assembly_source);
+    if (!source_copy) {
+        perror("strdup");
+        return -1;
     }
-}
-
-int32_t token_toint(const char* token) {
-    int32_t size = token_size(token);
-    int32_t value = 0;
-    for (int32_t i = 0; i < size; i++) {
-        value = value * 10 + (token[i] - '0');
-    }
-    return value;
-}
-
-int32_t token_equal(const char* a, const char* b) {
-    int32_t a_size = token_size(a);
-    int32_t b_size = token_size(b);
-    if (a_size != b_size) {
-        return 0;
-    }
-    for (int32_t i = 0; i < a_size; i++) {
-        if (a[i] != b[i]) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-const char* token_skipspace(const char* token) {
-    while (*token == ' ' || *token == '\n' || *token == '\r' || *token == '\t') {
-        token++;
-    }
-    return token;
-}
-
-const char* token_next(const char* token) {
-    int32_t size = token_size(token);
-    token += size;
-    token = token_skipspace(token);
-    while (token_equal(token, "//")) {
-        while (*token != '\n' && *token != '\0') {
-            token++;
-        }
-        token = token_skipspace(token);
-    }
-    return token;
-}
-
-void compile() {
-    // Load source file
-    FILE* src_file = fopen(LKJSCRIPT_SRCPATH, "rb");
-    if (!src_file) {
-        fprintf(stderr, "Error: Unable to open source file %s\n", LKJSCRIPT_SRCPATH);
-        exit(1);
-    }
-    int32_t src_size = fread(lkjscript.data + 1, 1, LKJSCRIPT_SIZE, src_file);
-    fclose(src_file);
-    lkjscript.data[0] = ' ';
-    lkjscript.data[src_size + 1] = '\0';
-
-    // Tokenize and compile
-    int32_t* inst_data = (int32_t*)(lkjscript.data + src_size + 2);
-    int32_t inst_size = 0;
-    const char* token_itr = (const char*)lkjscript.data;
-    token_itr = token_next(token_itr);
-    while (*token_itr != '\0') {
-        if (token_equal(token_itr, "debug")) {
-            token_itr = token_next(token_itr);
-            inst_data[inst_size++] = (INST_DEBUG << 24);
+    int code_index = 0;
+    char* line = strtok(source_copy, "\n");
+    while (line) {
+        while (*line == ' ' || *line == '\t'){
+            line++;}
+        if (strlen(line) == 0) {
+            line = strtok(NULL, "\n");
             continue;
         }
-        if (token_equal(token_itr, "imm")) {
-            token_itr = token_next(token_itr);
-            int32_t reg = token_toint(token_itr);
-            token_itr = token_next(token_itr);
-            int32_t value = token_toint(token_itr);
-            token_itr = token_next(token_itr);
-            inst_data[inst_size++] = (INST_IMM << 24) | (reg << 16) | value;
-            continue;
+        char mnemonic[16] = {0};
+        sscanf(line, "%15s", mnemonic);
+        uint32_t instruction = 0;
+        long imm = 0;
+        if (strcmp(mnemonic, "RET") != 0) {
+            char* immediate_part = strrchr(line, '#');
+            if (!immediate_part) {
+                fprintf(stderr, "Error: Immediate value starting with '#' not found in line: '%s'\n", line);
+                free(source_copy);
+                return -1;
+            }
+            char* endptr;
+            imm = strtol(immediate_part + 1, &endptr, 0);
+            if (endptr == immediate_part + 1) {
+                fprintf(stderr, "Error: Invalid immediate value in line: '%s'\n", line);
+                free(source_copy);
+                return -1;
+            }
         }
-        if (token_equal(token_itr, "copy")) {
-            token_itr = token_next(token_itr);
-            int32_t reg_dst = token_toint(token_itr);
-            token_itr = token_next(token_itr);
-            int32_t reg_src = token_toint(token_itr);
-            token_itr = token_next(token_itr);
-            inst_data[inst_size++] = (INST_COPY << 24) | (reg_dst << 16) | reg_src;
-            continue;
+        if (strcmp(mnemonic, "MOV") == 0) {
+            if (imm < 0 || imm > 65535) {
+                fprintf(stderr, "Error: Immediate for MOV must be between 0 and 65535. Line: '%s'\n", line);
+                free(source_copy);
+                return -1;
+            }
+            instruction = 0xD2800000 | ((uint16_t)imm << 5);
+        } else if (strcmp(mnemonic, "ADD") == 0) {
+            if (imm < 0 || imm > 4095) {
+                fprintf(stderr, "Error: Immediate for ADD must be between 0 and 4095. Line: '%s'\n", line);
+                free(source_copy);
+                return -1;
+            }
+            instruction = 0x91000000 | ((uint16_t)imm << 10);
+        } else if (strcmp(mnemonic, "SUB") == 0) {
+            if (imm < 0 || imm > 4095) {
+                fprintf(stderr, "Error: Immediate for SUB must be between 0 and 4095. Line: '%s'\n", line);
+                free(source_copy);
+                return -1;
+            }
+            instruction = 0xD1000000 | ((uint16_t)imm << 10);
+        } else if (strcmp(mnemonic, "RET") == 0) {
+            instruction = 0xD65F03C0;
+        } else {
+            fprintf(stderr, "Error: Unknown instruction or format: '%s'\n", line);
+            free(source_copy);
+            return -1;
         }
-        fprintf(stderr, "Error: Unknown token '%.*s' token[0]: %d\n", token_size(token_itr), token_itr, token_itr[0]);
-        exit(1);
+        memcpy(&code_buf[code_index], &instruction, sizeof(uint32_t));
+        code_index += sizeof(uint32_t);
+        line = strtok(NULL, "\n");
     }
-    inst_data[inst_size++] = (INST_END << 24);
-
-    // Move instructions to the start of lkjscript.data
-    memmove(lkjscript.data, inst_data, inst_size * sizeof(int32_t));
+    free(source_copy);
+    return code_index;
 }
 
-void run() {
-    int64_t reg[16];
-    uint8_t* pc = lkjscript.data;
-    while (1) {
-        uint32_t inst = *((uint32_t*)pc);
-        pc += 4;
-        switch (inst >> 24) {
-            case INST_END:
-                exit(0);
-            case INST_DEBUG: {
-                printf("Debug: reg[0]=%ld\n", reg[0]);
-                fflush(stdout);
-            } break;
-            case INST_IMM: {
-                uint32_t reg_id = (inst >> 16) & 0xFF;
-                uint32_t value = inst & 0xFFFF;
-                reg[reg_id] = value;
-            } break;
-            case INST_COPY: {
-                uint32_t reg_dst = (inst >> 16) & 0xFF;
-                uint32_t reg_src = inst & 0xFFFF;
-                reg[reg_dst] = reg[reg_src];
-            } break;
-        }
+int execute_code(const unsigned char* machine_code, int code_size) {
+    void* mem = mmap(NULL, code_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (mem == MAP_FAILED) {
+        perror("mmap");
+        return -1;
     }
+    memcpy(mem, machine_code, code_size);
+    if (mprotect(mem, code_size, PROT_READ | PROT_EXEC) == -1) {
+        perror("mprotect");
+        munmap(mem, code_size);
+        return -1;
+    }
+    __builtin___clear_cache((char*)mem, (char*)mem + code_size);
+    int (*func)() = (int (*)())mem;
+    int result = func();
+    munmap(mem, code_size);
+    return result;
 }
 
 int main() {
-    compile();
-    run();
+    const char* simple_assembly =
+        "MOV x0, #100\n"
+        "ADD x0, x0, #20\n"
+        "SUB x0, x0, #3\n"
+        "RET\n";
+    printf("--- Original Assembly Code (for ARM64) ---\n");
+    printf("%s", simple_assembly);
+    printf("-----------------------------------------\n\n");
+    unsigned char machine_code[4096];
+    int code_size = assemble_arm64(machine_code, simple_assembly);
+    if (code_size < 0) {
+        fprintf(stderr, "Assembly failed.\n");
+        return 1;
+    }
+    printf("--- Assembled Machine Code (ARM64) ---\n");
+    for (int i = 0; i < code_size; ++i) {
+        printf("%02X ", machine_code[i]);
+        if ((i + 1) % 4 == 0)
+            printf("\n");
+    }
+    printf("--------------------------------------\n\n");
+
+    int result = execute_code(machine_code, code_size);
+    printf("Expected result: 100 + 20 - 3 = 117\n");
+    printf("Execution result (Value of x0): %d\n", result);
+
+    return 0;
 }
