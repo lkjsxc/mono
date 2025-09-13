89a864612a7a5fac1ef6e3b965f6b15f2f00c097
lkjsxc <lkjsxc@gmail.com>
2025-09-14 08:17:54 +0900

    feat: Enhance parsing capabilities and improve token comparison functions in lkjscript

--- lkjscript/src/lkjscript.c
+++ lkjscript/src/lkjscript.c
@@ -27,6 +27,13 @@ enum node_type_t {
     NODE_TYPE_IDENT,
     NODE_TYPE_NUM,
     NODE_TYPE_STR,
+    NODE_TYPE_BF_BLOCK_LOOP,
+    NODE_TYPE_BF_PLUS,
+    NODE_TYPE_BF_MINUS,
+    NODE_TYPE_BF_LSHIFT,
+    NODE_TYPE_BF_RSHIFT,
+    NODE_TYPE_BF_OUTPUT,
+    NODE_TYPE_BF_INPUT,
 };
 
 union uni64_t {
@@ -54,11 +61,36 @@ struct node_t {
 
 struct result_parse_t {
     result_t result;
-    char* src_itr;
+    token_t* token_itr;
     node_t* node_itr;
 };
 
 uint8_t* mem = NULL;
+
+int64_t token_equal(token_t* a, token_t* b) {
+    if (a->size != b->size) {
+        return 0;
+    }
+    for (int64_t i = 0; i < a->size; i++) {
+        if (a->data[i] != b->data[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int64_t token_equal_str(token_t* a, const char* b) {
+    int64_t b_size = strlen(b);
+    if (a->size != b_size) {
+        return 0;
+    }
+    for (int64_t i = 0; i < a->size; i++) {
+        if (a->data[i] != b[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 result_t mem_init() {
     mem = mmap(NULL, LKJSCRIPT_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -117,9 +149,11 @@ result_t compile_readsrc(uint8_t** itr) {
         return RESULT_ERROR;
     }
     fclose(fp);
-    mem[fsize] = '\n';
-    mem[fsize + 1] = '\0';
-    *itr = mem + fsize + 2;
+    mem[fsize] = '\0';
+    *itr = mem + fsize + 1;
+    // mem[fsize] = '\n';
+    // mem[fsize + 1] = '\0';
+    // *itr = mem + fsize + 2;
     return RESULT_OK;
 }
 
@@ -147,14 +181,86 @@ result_t compile_tokenize(uint8_t** itr, char* src) {
     return RESULT_OK;
 }
 
+node_t* compile_parse_push(node_t** node_itr, node_type_t type, node_t* parent) {
+    node_t* node = *node_itr;
+    (*node_itr) += 1;
+    node->type = type;
+    node->value.u64 = 0;
+    node->next = NULL;
+    node->parent = parent;
+    node->child_begin = NULL;
+    node->child_rbegin = NULL;
+    if (parent->child_begin == NULL) {
+        parent->child_begin = node;
+    } else {
+        parent->child_rbegin->next = node;
+    }
+    parent->child_rbegin = node;
+    return node;
+}
+
 result_parse_t compile_parse_stmt(node_t* parent, token_t* token_itr, node_t* node_itr) {
+    if (token_equal_str(token_itr, "[")) {
+        node_t* loop_parent = compile_parse_push(&node_itr, NODE_TYPE_BF_BLOCK_LOOP, parent);
+        token_itr += 1;
+        while (!token_equal_str(token_itr, "]")) {
+            result_parse_t result = compile_parse_stmt(loop_parent, token_itr, node_itr);
+            if (result.result != RESULT_ERROR) {
+                fprintf(stderr, "Error: I will think about error message.\n");
+                return (result_parse_t){.result = RESULT_ERROR, .token_itr = token_itr, .node_itr = node_itr};
+            }
+            token_itr = result.token_itr;
+            node_itr = result.node_itr;
+        }
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, "+")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_PLUS, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, "-")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_MINUS, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, "<")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_LSHIFT, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, ">")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_RSHIFT, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, ".")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_OUTPUT, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else if (token_equal_str(token_itr, ",")) {
+        node_t* plus = compile_parse_push(&node_itr, NODE_TYPE_BF_INPUT, parent);
+        token_itr += 1;
+        return (result_parse_t){.result = RESULT_OK, .token_itr = token_itr, .node_itr = node_itr};
+    } else {
+        fprintf(stderr, "Error: I will think about error message.\n");
+        return (result_parse_t){.result = RESULT_ERROR, .token_itr = token_itr, .node_itr = node_itr};
+    }
 }
 
 result_t compile_parse(uint8_t** itr, token_t* token) {
     token_t* token_itr = token;
     node_t* node_itr = (node_t*)*itr;
     node_t* root = node_itr++;
-    while (token_itr != '\0') {
+    root->type = NODE_TYPE_BLOCK;
+    root->value.u64 = 0;
+    root->next = NULL;
+    root->parent = NULL;
+    root->child_begin = NULL;
+    root->child_rbegin = NULL;
+    while (token_itr->data != NULL) {
+        result_parse_t result = compile_parse_stmt(root, token_itr, node_itr);
+        if (result.result != RESULT_OK) {
+            fprintf(stderr, "Error: I will think about error message.\n");
+        }
+        token_itr = result.token_itr;
+        node_itr = result.node_itr;
     }
     *itr = (uint8_t*)node_itr;
     printf("Parsing step (stub)... OK\n");
@@ -162,7 +268,9 @@ result_t compile_parse(uint8_t** itr, token_t* token) {
 }
 
 result_t compile_codegen(uint8_t** itr, node_t* node_root) {
-    // arm64 linux
+    uint32_t* code_itr = (uint32_t*)itr;
+
+    *itr = (uint8_t*)code_itr;
     return RESULT_OK;
 }
 
